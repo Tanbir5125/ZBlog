cb5619a7f7ee00995fc454c638810c66abbf0271
TANBIR AHAMED <tanbirahamedg@gmail.com>
2024-09-26 21:33:11 +0530

    added new code files

--- index.js
+++ index.js
@@ -7,8 +7,35 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+let blogs = [];
+
 app.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', {
+        blogs: blogs,
+    });
+});
+
+app.post('/', (req, res) => {
+    const newBlog = {
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author,
+    };
+
+    // Check if the blog already exists
+    const existingBlog = blogs.find(blog => blog.title === newBlog.title);
+
+    if (!existingBlog) {
+        blogs.push(newBlog);
+    }
+
+    res.render('index.ejs', {
+        blogs: blogs,
+    });
+});
+
+app.get('/write-a-blog', (req, res) => {
+    res.render('writeBlog.ejs');
 });
 
 app.listen(port, () => {
